0ce91a91c3f0b94951da238124401f042596e776
j4ckxyz <>
2025-11-25 09:35:35 +0000

    Fix: Remove duplicate imports in bot.js
    Removed accidentally duplicated import statements causing syntax error

--- src/bot.js
+++ src/bot.js
@@ -2,56 +2,6 @@ import 'dotenv/config';
 import { Client, GatewayIntentBits, REST, Routes } from 'discord.js';
 import logger from './utils/logger.js';
 import summariseCommand from './commands/summarise.js';
-
-// Validate environment variables
-if (!process.env.DISCORD_BOT_TOKEN) {
-  logger.error('DISCORD_BOT_TOKEN is required in .env file');
-  process.exit(1);
-}
-
-if (!process.env.LLM_API_KEY) {
-  logger.error('LLM_API_KEY is required in .env file');
-  process.exit(1);
-}
-
-// Create Discord client
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]
-});
-
-// Commands collection
-const commands = [summariseCommand];
-
-// Register slash commands
-async function registerCommands() {
-  const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
-  
-  try {
-    logger.info('Started refreshing application (/) commands.');
-
-    const commandsData = commands.map(cmd => cmd.data.toJSON());
-    
-    logger.info(`Registering ${commandsData.length} command(s): ${commandsData.map(c => c.name).join(', ')}`);
-
-    await rest.put(
-      Routes.applicationCommands(client.user.id),
-      { body: commandsData }
-    );
-
-    logger.info('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    logger.error('Error registering commands:', error);
-  }
-}
-
-import 'dotenv/config';
-import { Client, GatewayIntentBits, REST, Routes } from 'discord.js';
-import logger from './utils/logger.js';
-import summariseCommand from './commands/summarise.js';
 import rateLimitService from './services/ratelimit.js';
 import summariserService from './services/summariser.js';
 
@@ -158,7 +108,7 @@ client.once('ready', async () => {
   await registerCommands();
   
   logger.info('Bot is ready to summarise!');
-  logger.info('Commands: /summarise, !summarise, @mention');
+  logger.info('Commands: /summarise, !summarise, !summary, @mention');
 });
 
 // Event: Interaction created (slash commands)
